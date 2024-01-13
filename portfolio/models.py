b25e1a2336f174d99aa43f6b518abba3d5a80ce8
Kabir <kaiisenseii465@gmail.com>
2024-01-13 10:02:41 +0545

    create a new project of portfolio

---
+++ portfolio/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
---
+++ portfolio/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from . import views
+
+urlpatterns = [
+    path('', views.home , name='index')
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
