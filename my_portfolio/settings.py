5911958571557925ca9ea9103ac0280ecf99ef85
Kabir <kaiisenseii465@gmail.com>
2024-01-13 19:11:53 +0545

    changes done

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -121,9 +121,22 @@ STATIC_URL = 'static/'
 
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = ((BASE_DIR, 'static'))
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#media
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from django.contrib import admin
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
+    # path('', include(('Authentication.urls', 'authentication'), namespace='authentication')),
+    # path('', include(('Dashboard.urls', 'dashboard'), namespace='dashboard')),
     path('', views.home , name='index')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
